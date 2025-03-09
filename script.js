f0053a9b57e89a9e33fa72ce8b7b3cef0122aeb8
Mulisa Mudau <115104071+Mulisa3@users.noreply.github.com>
2025-03-09 23:19:36 +0200

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your message! We will respond shortly.');
+    this.reset();
+});
